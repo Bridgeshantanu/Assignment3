bc66e1c41f956fd3a7792bea42ee9a814dbecdcf
“Bridgeshantanu” <shantanupadul@gmail.com>
2023-10-02 18:50:38 +0530

    [Shantanu]: basic code is added

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //constructor
+            //Student student = new Student();
+
+            //Person person = new Person("Sarthak", 25);
+            //Console.WriteLine(person.Name);
+            //Console.WriteLine(person.Age);
+
+
+
+            //interface
+            //IShape circle = new Circle(5.0);
+            //IShape rectangle = new Rectangle(4.0, 3.0);
+
+            //Console.WriteLine("Circle Area: " + circle.CalculateArea());
+            //Console.WriteLine("Rectangle Area: " + rectangle.CalculateArea());
+
+
+
+            //access modifiers
+           
+            MyClass myObject = new MyClass();
+            myObject.MyField = 42; 
+            myObject.MyMethod();   
+            Console.WriteLine(myObject.MyField); 
+
+            
+            MyClass1 myObject1 = new MyClass1();
+            myObject1.AccessPrivateMembers();
+
+
+            MyDerivedClass derivedObject = new MyDerivedClass();
+            derivedObject.AccessProtectedField();
+            int value = derivedObject.GetProtectedField();
+            Console.WriteLine("Value of myProtectedField: " + value);
+
+
+            AnotherClass anotherObject = new AnotherClass();
+            anotherObject.AccessInternalField();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    internal class Student
+    {
+        public Student()
+        {
+            Console.WriteLine("ram");
+        }
+    }
+
+
+    class Person
+    {
+        public string Name;
+        public int Age;
+        public Person(string name , int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+
+
+
+    //interface
+    interface IShape
+    {
+        double CalculateArea();
+    }
+
+   class Circle : IShape
+   {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * radius * radius;
+        }
+   }
+
+    class Rectangle : IShape
+    {
+        private double width;
+        private double height;
+
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public double CalculateArea()
+        {
+            return width * height;
+        }
+    }
+
+
+    //access modifiers
+    public class MyClass
+    {
+        public int MyField; 
+
+        public void MyMethod() 
+        {
+            
+        }
+    }
+
+    class MyClass1
+    {
+        private int myPrivateField;
+
+        private void MyPrivateMethod() 
+        {
+            Console.WriteLine("Private method called.");
+        }
+
+        public void AccessPrivateMembers()
+        {
+           
+            myPrivateField = 45;
+            MyPrivateMethod();
+
+            Console.WriteLine($"Private field value: {myPrivateField}");
+        }
+    }
+
+
+    class MyBaseClass
+    {
+        protected int myProtectedField; 
+    }
+
+    class MyDerivedClass : MyBaseClass
+    {
+        public void AccessProtectedField()
+        {
+            myProtectedField = 52; 
+        }
+
+        public int GetProtectedField()
+        {
+            return myProtectedField; 
+        }
+    }
+
+
+    internal class MyInternalClass
+    {
+        internal int MyInternalField; 
+    }
+
+    internal class AnotherClass
+    {
+        public void AccessInternalField()
+        {
+            MyInternalClass myInternalObject = new MyInternalClass();
+            myInternalObject.MyInternalField = 70; 
+            Console.WriteLine("Value of MyInternalField: " + myInternalObject.MyInternalField);
+        }
+    }
+}
